33fb8554ea4c4180b262566aae40b78bad438849
qwe624758472 <624758472@qq.com>
2021-04-08 17:51:15 +0800

    加入unity3d制作的fps客户端

---
+++ fps/Assets/FPS/Scripts/Game/Managers/ActorsManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity.FPS.Game
+{
+    public class ActorsManager : MonoBehaviour
+    {
+        public List<Actor> Actors { get; private set; }
+        public GameObject Player { get; private set; }
+
+        public void SetPlayer(GameObject player) => Player = player; //匿名函数 =>
+
+        void Awake()
+        {
+            Actors = new List<Actor>();
+        }
+    }
+}
